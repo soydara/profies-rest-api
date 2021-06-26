31e78ec4b3f67440ef9ae3b7a74a0f581fe8acd1
soydara <clear>
2021-06-26 13:52:18 +0700

    push code api

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-	'rest_framework.authtoken',
-	'profiles_api',
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
